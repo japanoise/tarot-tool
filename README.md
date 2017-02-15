0b813c0566133385e6917412c92ba37cbbbad85e
japanoise <japanoise@tfwno.gf>
2017-02-15 19:50:00 +0000

    add celtic cross spread

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # tarot-tool
 
 This tool will generate tarot spreads. Right now it will generate a linear collage of
-cards given, à la [the three card spread](https://www.daily-tarot-girl.com/tarot-card-spreads/3-card-tarot-spread-past-present-future/)
+cards given, à la [the three card spread,](https://www.daily-tarot-girl.com/tarot-card-spreads/3-card-tarot-spread-past-present-future/)
+or (if passed the -c option) a [Celtic Cross](http://www.learntarot.com/ccross.htm)
+spread.
 
 ## Usage
 
@@ -9,6 +11,7 @@ Run `./tarot-tool [cards...]`
 
 ~~~~~~
 Usage of ./tarot-tool:
+  -c	use a celtic cross layout
   -g int
     	pixels of spacing glue between edges (default 30)
 ~~~~~~
@@ -20,6 +23,12 @@ Usage of ./tarot-tool:
 `./tarot-tool rws/wa09.jpg rws/cu07.jpg rws/ar11.jpg`
 
 ![An example image showing a three-card spread](example/ex1.png)
+
+### Celtic Cross spread
+
+`./tarot-tool -c rws/wa09.jpg rws/cu07.jpg rws/ar11.jpg rws/pe08.jpg rws/ar12.jpg rws/cuac.jpg rws/swac.jpg rws/wa10.jpg rws/ar00.jpg rws/ar17.jpg`
+
+![An example image showing a Celtic Cross spread](example/ex2.png)
 
 ## Licensing
 
--- main.go
+++ main.go
@@ -14,25 +14,72 @@ import (
 
 func main() {
 	glue := flag.Int("g", 30, "pixels of spacing glue between edges")
+	cc := flag.Bool("c", false, "use a celtic cross layout")
 	flag.Parse()
 	images := make([]image.Image, 0, 3)
 	for _, arg := range flag.Args() {
 		images = append(images, readimg(arg))
 	}
-	canvas, cw, _ := gencanvas(images, *glue)
-	for i, img := range images {
-		anchor := ((i + 1) * *glue) + (i * cw)
-		for x := 0; x < img.Bounds().Dx(); x++ {
-			for y := 0; y < img.Bounds().Dy(); y++ {
-				canvas.Set(anchor+x, *glue+y, img.At(x, y))
-				//log.Print(canvas.At(anchor + x, *glue + y))
-			}
-		}
+	var m image.RGBA
+	if *cc {
+		m = ccspread(images, glue)
+	} else {
+		m = linearspread(images, glue)
 	}
-	writeimg(canvas, fmt.Sprintf("%d.%s", int32(time.Now().Unix()), "png"))
+	writeimg(m, fmt.Sprintf("%d.%s", int32(time.Now().Unix()), "png"))
 }
 
-func gencanvas(images []image.Image, glue int) (image.RGBA, int, int) {
+func ccspread(images []image.Image, glue *int) image.RGBA {
+	for len(images) < 10 {
+		images = append(images, getunknown())
+	}
+	cardwidth, cardheight := getcwch(images)
+	g := *glue
+	canvas := *image.NewRGBA(image.Rect(0, 0, (3*cardwidth)+cardheight+(5*g), (5*g)+(4*cardheight)))
+	cssize := (cardheight - cardwidth) / 2
+	canchx := (2 * g) + cardwidth + cssize
+	incy := cardheight + g
+	copycard(images[4], canvas, canchx, g)
+	copycard(images[0], canvas, canchx, g+incy)
+	copycard(images[2], canvas, canchx, g+(2*incy))
+	copycard(images[3], canvas, g, g+incy)
+	copycard(rotatecard(images[1]), canvas, canchx-cssize, g+incy+cssize)
+	copycard(images[5], canvas, canchx+cardwidth+cssize+g, g+incy)
+	ranchx := canchx + (2 * cardwidth) + cssize + (3 * g)
+	copycard(images[9], canvas, ranchx, g)
+	copycard(images[8], canvas, ranchx, g+incy)
+	copycard(images[7], canvas, ranchx, g+(2*incy))
+	copycard(images[6], canvas, ranchx, g+(3*incy))
+	return canvas
+}
+
+func rotatecard(img image.Image) image.Image {
+	retval := image.NewRGBA(image.Rect(0, 0, img.Bounds().Dy(), img.Bounds().Dx()))
+	for x := 0; x < img.Bounds().Dx(); x++ {
+		for y := 0; y < img.Bounds().Dy(); y++ {
+			retval.Set(y, x, img.At(x, y))
+		}
+	}
+	return retval
+}
+
+func copycard(img image.Image, canvas image.RGBA, anchorx, anchory int) {
+	for x := 0; x < img.Bounds().Dx(); x++ {
+		for y := 0; y < img.Bounds().Dy(); y++ {
+			canvas.Set(anchorx+x, anchory+y, img.At(x, y))
+		}
+	}
+}
+
+func linearspread(images []image.Image, glue *int) image.RGBA {
+	canvas, cw, _ := gencanvas(images, *glue)
+	for i, img := range images {
+		copycard(img, canvas, ((i+1)**glue)+(i*cw), *glue)
+	}
+	return canvas
+}
+
+func getcwch(images []image.Image) (int, int) {
 	cardwidth, cardheight := 0, 0
 	for _, img := range images {
 		if img.Bounds().Dx() > cardwidth {
@@ -42,6 +89,11 @@ func gencanvas(images []image.Image, glue int) (image.RGBA, int, int) {
 			cardheight = img.Bounds().Dy()
 		}
 	}
+	return cardwidth, cardheight
+}
+
+func gencanvas(images []image.Image, glue int) (image.RGBA, int, int) {
+	cardwidth, cardheight := getcwch(images)
 	l := len(images)
 	return *image.NewRGBA(image.Rect(0, 0, ((l+1)*glue)+(l*cardwidth), (2*glue)+cardheight)), cardwidth, cardheight
 }
